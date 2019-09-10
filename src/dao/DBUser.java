3e6436b81bcc99c5f1066a69695f509059c11d7a
Martina <martinadem97@gmail.com>
2019-09-10 15:04:06 +0200

    Merge branch 'master' of D:\workspace\intelliJ\helloWorld with conflicts.

--- src/dao/DBUser.java
+++ src/dao/DBUser.java
@@ -2,98 +2,16 @@ package dao;
 
 import entity.User;
 
-import java.sql.*;
-
 public class DBUser {
-    private static String USER = "root";    //user db
-    private static String PASS = "root";    //password db
 
-    private static final String DB_URL = "jdbc:mariadb://127.0.0.1:3406/provadb";   //indirizzi db
-    private static final String DRIVER_CLASS_NAME = "org.mariadb.jdbc.Driver";
-
-    public static User insertNewUser() {
-
-        // STEP 1: dichiarazioni
-        Statement stmt = null;
-        Connection conn = null;
-        User u = null;
-        try {
-            // STEP 2: loading dinamico del driver mysql
-            Class.forName(DRIVER_CLASS_NAME);
-
-            // STEP 3: apertura connessione
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);     //connessione con database specificato prima
-
-            // STEP 4: creazione ed esecuzione della query
-            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,  //ResultSet è oggetto che contiene il risultato della query
-                    ResultSet.CONCUR_READ_ONLY);
+    private  final String USER = "root";
+    private  final String PASS = "root";
+    private  final String DB_URL = "jdbc:mysql://127.0.0.1:3306/localDB";
+    private  final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
 
 
-            String query = "INSERT INTO utenti VALUES ('ceci','bho','root2','root2');";
+    public static User findByNameAndPassword {
 
-            /*
-            String sql = "SELECT nome, cognome, username, " +             //scrivo query in SQL
-                    "password FROM utenti where username = '"
-                    + username + "' AND password = '" + password + "';";
-            */
+    }
 
-            ResultSet rs = stmt.executeQuery(query);                        //eseguo query
-
-            if (!rs.first()) // rs not empty
-                return null;
-
-            boolean moreThanOne = rs.first() && rs.next();                  //mette cursore alla prima riga
-            assert !moreThanOne; // per abilitare le asserzioni, avviare la JVM con il parametro -ea
-            // (Run Configurations -> <configurazione utilizzata per l'avvio del server> -> Arguments -> VM Arguments).
-            // N.B. Le asserzioni andrebbero usate solo per test e debug, non per codice in produzione
-
-            // riposizionamento del cursore
-            rs.first();
-
-            // lettura delle colonne "by name"
-            String nome = rs.getString("nome");
-            String cognome = rs.getString("cognome");
-            String usernameLoaded = rs.getString("username");
-
-            //assert (usernameLoaded.equals(username));
-
-            u = new User(usernameLoaded, "",
-                    nome, cognome);
-
-            // STEP 6: Clean-up dell'ambiente
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            // Errore durante l'apertura della connessione
-            se.printStackTrace();
-        } catch (Exception e) {
-            // Errore nel loading del driver
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    stmt.close();
-            } catch (SQLException se2) {
             }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-
-        return u;
-    }
-
-    public static User findByNameAndPasswordMockup(String username, String password) {
-        if ("myusername".equals(username) && "mypassword".equals(password))
-            return new User("myusername", "", "Tizio","Caio");
-        else return null;
-    }
-
-    public static void main(String[] args){
-        User myUser = insertNewUser();
-    }
-}
--- src/dao/DBconnection.java
+++ src/dao/DBconnection.java
@@ -4,39 +4,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBconnection {
+public class DBHelper {
 
-    private final String USER = "root";
-    private final String PASS = "qwerty18";
-    private final String DB_URL = "jdbc:mysql://127.0.0.1:3306/db_appartamenti";
-    private final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private  final String USER = "root";
+    private  final String PASS = "root";
+    private  final String DB_URL = "jdbc:mysql://127.0.0.1:3306/localDB";
+    private  final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
 
     private Connection connection = null;
 
-    public DBconnection(){}
+    // Apertura connessione con il DB
 
-    // Operazione per aprire la connessione con il DB
     public Connection openConnection() {
         if (connection != null) return connection;
         try {
             Class.forName(DRIVER_CLASS_NAME);
             connection = DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (SQLException e) {
-            System.out.println("Connection exception");
+            System.out.println("There was an exception during connection opening !");
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
-            System.out.println("Driver Exception");
+            System.out.println("There was an exception loading drivers!");
             e.printStackTrace();
         }
         return connection;
     }
 
-    // Operazione per chiudere la connessione con il DB
+    // Chiusura la connessione con il DB
+
     public void closeConnection(){
         try {
             connection.close();
         } catch (SQLException e) {
-            System.out.println("Connection exception");
+            System.out.println("There was an exception during connection close !");
             e.printStackTrace();
         }
     }
